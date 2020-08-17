b91f13cd967a5a29a238b9326f8e4d3f2c473e37
Rian1010 <rtegally1098@gmail.com>
2020-08-18 00:14:56 +0200

    Made a few adjustments in the design

--- SkywheelersProject/settings.py
+++ SkywheelersProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'b973e28453ce.ngrok.io']
 
 
 # Application definition
