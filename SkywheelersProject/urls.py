8083c3d7bd44aec4fe003b5f8a893bde3d47b47f
Tim Diedrich <s2197581@stud.uni-frankfurt.de>
2022-11-13 15:38:08 +0100

    Minor changes and example sponsor pictures

--- SkywheelersProject/urls.py
+++ SkywheelersProject/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     path('gallerie/', include('gallery.urls')),
     path('fanshop/', include('fanshop.urls')),
     path('live_stream/', include('livestream.urls')),
-    path('kontakte/', include('contact.urls'))
+    path('kontakt/', include('contact.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
