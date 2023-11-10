414907475b71841577b75c3ade3a0996099cb283
Tim Diedrich <s2197581@stud.uni-frankfurt.de>
2023-11-10 19:18:29 +0100

    sponsoren styling

---
+++ home/static/home/sponsors.js
@@ -0,0 +1,16 @@
+const sponsors = document.querySelector('.sponsor-sec');
+
+let rect = sponsors.getBoundingClientRect(),
+    sponsorsTop = rect.top,
+    oldScrollY = window.scrollY;
+
+document.addEventListener('scroll', () => {
+    if (oldScrollY < window.scrollY && window.pageYOffset > sponsorsTop / 3) {
+        sponsors.scrollLeft += 11;
+    } else if (window.scrollY < oldScrollY) {
+        sponsors.scrollLeft -= 11;
+    }
+    oldScrollY = window.scrollY;
+});
+
+console.log("Test");
